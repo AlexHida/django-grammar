b297920688e9ced3c2b0d3a2c3f48e3d1b84025b
AlexHida <96128820+AlexHida@users.noreply.github.com>
2023-08-09 16:58:21 -0500

    comment setting and requirements

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,9 +81,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgrsql://postgres:postgres@localhost:5432/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
+
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
